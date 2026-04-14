1e9ead8bee94d189b3e084542fa9f00582ab4a0c
lincube <lincube3@hotmail.com>
2026-04-14 12:25:28 +0800

    feat.SDK加入了FA的引用。

---
+++ LanMountainDesktop.PluginSdk/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using Avalonia.Metadata;
+
+[assembly: XmlnsPrefix("http://lanmountain.tech/schemas/xaml/sdk", "lmd")]
+[assembly: XmlnsDefinition("http://lanmountain.tech/schemas/xaml/sdk", "LanMountainDesktop.PluginSdk")]
